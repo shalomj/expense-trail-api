1aa435883dbaedc46f909bd2e6653c2d3cc7479d
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-04-15 18:47:52 +0800

    Added user model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
+    },
+    email: {
+        type: String,
+        required: true,
+
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    photoUrl: {
+        type: String
+    },
+    emailVerified: {
+        type: Boolean
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
