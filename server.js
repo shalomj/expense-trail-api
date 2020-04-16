70f359e495c0a1f33ea05862463902deee37a3a0
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2020-04-16 17:10:34 +0800

    Added auth API endpoint and fixed error handlers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const DB = require('./db');
 
 // Routes
 const categoriesRoute = require('./routes/categories');
+const authRoute = require('./routes/auth');
 
 // Init environment config
 dotenv.config();
@@ -19,23 +20,23 @@ app.use(express.json());
 
 // API routes
 app.use('/api/v1/categories', categoriesRoute);
+app.use('/api/v1/auth', authRoute);
 
 // 404 Not Found handler
 app.use((req, res, next) => {
-    const err = new Error('404 Not Found');
-
-    err.status = 404;
-
-    next(err);
+    next({
+        status: 404,
+        message: '404 Not Found'
+    });
 });
 
 // Default error handler
 app.use((err, req, res, next) => {
-    res.status = err.status || 500;
-    res.send({
-        status: "failed",
-        message: err.message
-    });
+    res.status(err.status || 500)
+        .json({
+            status: "failed",
+            message: err.message
+        });
 });
 
 const PORT = process.env.PORT || 5000;
